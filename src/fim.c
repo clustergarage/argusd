72aef878840ff4d664f6a0c0e0fd97fda782dfae
Brett Fowle <brettfowle@gmail.com>
2018-06-16 14:22:56 -0400

    add comments to fim.c

--- src/fim.c
+++ src/fim.c
@@ -13,26 +13,44 @@
   exit(EXIT_FAILURE); \
 } while (0)
 
+/**
+ * read all available inotify events from the file descriptor `fd`
+ * `wd` is the table of watch descriptors for the directories in `argv`
+ * `argc` is the length of `wd` and `argv`
+ * `argv` [1-N] is the list of watched directories
+ */
 static void handle_events(int fd, int *wd, int argc, char *argv[]) {
+  /**
+   * some systems cannot read integer variables if they are not properly aligned
+   * on other systems, incorrect alignment may decrease performance
+   * hence, the buffer used for reading from the inotify file descriptor should
+   * have the same alignment as struct inotify_event
+   */
   char buf[4096] __attribute__((aligned(__alignof__(struct inotify_event))));
   const struct inotify_event *event;
   int i;
   ssize_t len;
   char *ptr;
 
+  // loop while events can be read from the inotify file descriptor
   for (;;) {
+    // read some events
     len = read(fd, buf, sizeof(buf));
     if (len == -1 && errno != EAGAIN) {
       errExit("read");
     }
 
+    // if the non-blocking `read()` found no events to read, then it
+    // returns with -1 with `errno` set to `EAGAIN`; exit the loop
     if (len <= 0) {
       break;
     }
 
+    // loop over all events in the buffer
     for (ptr = buf; ptr < buf + len; ptr += sizeof(struct inotify_event) + event->len) {
       event = (const struct inotify_event *)ptr;
-      
+
+      // print event type
       if (event->mask & IN_OPEN) {
         printf("IN_OPEN: ");
       }
@@ -40,6 +58,7 @@ static void handle_events(int fd, int *wd, int argc, char *argv[]) {
         printf("IN_MODIFY: ");
       }
 
+      // print the name of the watched directory
       for (i = 1; i < argc; ++i) {
         if (wd[i] == event->wd) {
           printf("%s/", argv[i]);
@@ -47,10 +66,12 @@ static void handle_events(int fd, int *wd, int argc, char *argv[]) {
         }
       }
 
+      // print the name of the file
       if (event->len) {
         printf("%s", event->name);
       }
 
+      // print the type of filesystem object
       if (event->mask & IN_ISDIR) {
         printf(" [directory]\n");
       } else {
@@ -68,11 +89,11 @@ int main(int argc, char *argv[]) {
   struct pollfd fds[2];
 
   if (argc < 3) {
-    fprintf(stderr, "%s </proc/PID/ns/FILE> <paths...>\n", argv[0]);
+    fprintf(stderr, "%s </proc/PID/ns/NAMESPACE> <paths...>\n", argv[0]);
     exit(EXIT_FAILURE);
   }
 
-  printf("Press ENTER key to terminate.\n");
+  // -- JOIN THE NAMESPACE
 
   // get file descriptor for namespace
   fd = open(argv[1], O_RDONLY);
@@ -85,20 +106,27 @@ int main(int argc, char *argv[]) {
     errExit("setns");
   }
 
-  // execute a command in namespace
-  //execvp(argv[2], &argv[2]);
-  //errExit("execvp");
-    
+  // -- START THE INOTIFY WATCHER
+
+  printf("Press ENTER key to terminate.\n");
+
+  // create the file descriptor for accessing the inotify API
   fd = inotify_init1(IN_NONBLOCK);
   if (fd == -1) {
     errExit("inotify_init1");
   }
- 
+
+  // allocate memory for watch descriptors
   wd = calloc(argc, sizeof(int));
   if (wd == NULL) {
     errExit("calloc");
   }
-  
+
+  /**
+   * make directories for events
+   * - file was opened
+   * - file was modified
+   */
   for (i = 1; i < argc; i++) {
     wd[i] = inotify_add_watch(fd, argv[2], IN_OPEN | IN_MODIFY);
     if (wd[i] == -1) {
@@ -107,14 +135,16 @@ int main(int argc, char *argv[]) {
     }
   }
 
+  // prepare for polling
   nfds = 2;
-
+  // console input
   fds[0].fd = STDIN_FILENO;
   fds[0].events = POLLIN;
-
+  // inotify input
   fds[1].fd = fd;
   fds[1].events = POLLIN;
 
+  // wait for events and/or terminal input
   printf("Listening for events.\n");
   while (1) {
     poll_num = poll(fds, nfds, -1);
@@ -127,6 +157,7 @@ int main(int argc, char *argv[]) {
 
     if (poll_num > 0) {
       if (fds[0].revents & POLLIN) {
+        // console input is available; empty stdin and quit
         while (read(STDIN_FILENO, &buf, 1) > 0 && buf != '\n') {
           continue;
         }
@@ -134,6 +165,7 @@ int main(int argc, char *argv[]) {
       }
 
       if (fds[1].revents & POLLIN) {
+        // inotify events are available
         handle_events(fd, wd, argc, argv);
       }
     }
@@ -141,7 +173,9 @@ int main(int argc, char *argv[]) {
 
   printf("Listening for events stopped.\n");
 
+  // close inotify file descriptor
   close(fd);
   free(wd);
+
   exit(EXIT_SUCCESS);
 }
