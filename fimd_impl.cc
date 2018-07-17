ea24412fd6be50217eb0c8ab89c3c5d7f79d9fad
Brett Fowle <brettfowle@gmail.com>
2018-07-17 00:43:58 -0400

    support latest pb defs; prepare for multiple container watcher

--- fimd_impl.cc
+++ fimd_impl.cc
@@ -1,3 +1,4 @@
+#include <memory>
 #include <sstream>
 #include <string>
 #include <sys/eventfd.h>
@@ -19,12 +20,15 @@ using fim::Fimd;
 using fim::FimdConfig;
 using fim::FimdHandle;
 using fim::FimWatcherSubject;
+using fim::Empty;
 using grpc::ServerContext;
 using grpc::Status;
+using ::google::protobuf::RepeatedField;
+using ::google::protobuf::int32;
 
 Status FimdImpl::CreateWatch(ServerContext *context, const FimdConfig *request, FimdHandle *response) {
-    const string &containerId = FimdUtil::eraseSubstr(request->containerid(), "docker://");
-    int pid = FimdUtil::getPidForContainer(containerId);
+	// @TODO: multiple containerids passed
+    int pid = FimdUtil::getPidForContainer(cleanContainerId(request->containerid()));
     if (!pid) {
         return Status::CANCELLED;
     }
@@ -32,44 +36,47 @@ Status FimdImpl::CreateWatch(ServerContext *context, const FimdConfig *request, 
 	// find existing watcher by pid in case we need to update
 	// inotify_add_watcher is designed to both add and modify depending
 	// on if a fd exists already for this path
-    shared_ptr<FimdWatcher> watcher = findFimdWatcherByPid(pid);
+    shared_ptr<FimdHandle> watcher = findFimdWatcherByPid(request->hostuid(), pid);
 
-    vector<int> procFds;
+	RepeatedField<int32> procFds;
+	if (watcher != nullptr) {
+		//response = watcher.get();
+		procFds = *watcher->mutable_processeventfd();
+	}
 
-    for (int i = 0; i < request->subjects_size(); ++i) {
-        const FimWatcherSubject &subject = request->subjects(i);
-
+	for_each(request->subject().cbegin(), request->subject().cend(), [&](const FimWatcherSubject subject) {
 		cout << "[server] " << (watcher == nullptr ? "Starting" : "Updating") << " inotify watcher..." << endl;
 
 		if (watcher != nullptr) {
 			// stop existing watcher polling
 			sendKillSignalToWatcher(watcher);
 		}
-
 		createInotifyWatcher(subject, getPathArrayFromSubject(pid, subject),
 			getEventMaskFromSubject(subject), &procFds);
-    }
+    });
+
+	// set response object
+	response->set_hostuid(request->hostuid().c_str());
+	response->add_pid(pid);
+	for_each(procFds.cbegin(), procFds.cend(), [&](int procFd) {
+		response->add_processeventfd(procFd);
+	});
 
     if (watcher == nullptr) {
-		// create and store new watcher
-        FimdWatcher fw = {pid, procFds};
-        m_watchers.push_back(make_shared<FimdWatcher>(fw));
-    } else {
-		// update proc event fds on existing watcher
-		watcher->processEventfds = procFds;
+		// store new watcher
+		m_watchers.push_back(make_shared<FimdHandle>(*response));
     }
 
     return Status::OK;
 }
 
-Status FimdImpl::DestroyWatch(ServerContext *context, const FimdConfig *request, FimdHandle *response) {
-    const string &containerId = FimdUtil::eraseSubstr(request->containerid(), "docker://");
-    int pid = FimdUtil::getPidForContainer(containerId);
+Status FimdImpl::DestroyWatch(ServerContext *context, const FimdConfig *request, Empty *response) {
+    int pid = FimdUtil::getPidForContainer(cleanContainerId(request->containerid()));
     if (!pid) {
         return Status::CANCELLED;
     }
 
-    shared_ptr<FimdWatcher> watcher = findFimdWatcherByPid(pid);
+    shared_ptr<FimdHandle> watcher = findFimdWatcherByPid(request->hostuid(), pid);
     if (watcher != nullptr) {
 		sendKillSignalToWatcher(watcher);
     }
@@ -77,9 +84,13 @@ Status FimdImpl::DestroyWatch(ServerContext *context, const FimdConfig *request,
     return Status::OK;
 }
 
-shared_ptr<FimdImpl::FimdWatcher> FimdImpl::findFimdWatcherByPid(const int pid) {
-    auto it = find_if(m_watchers.cbegin(), m_watchers.cend(),
-        [&](shared_ptr<FimdWatcher> watcher) { return watcher->pid == pid; });
+shared_ptr<FimdHandle> FimdImpl::findFimdWatcherByPid(const string hostUid, const int pid) {
+    auto it = find_if(m_watchers.cbegin(), m_watchers.cend(), [&](shared_ptr<FimdHandle> watcher) {
+		auto pit = find_if(watcher->pid().cbegin(), watcher->pid().cend(),
+			[&](int p) { return p == pid; });
+		return watcher->hostuid() == hostUid &&
+			pit != watcher->pid().cend();
+	});
     if (it != m_watchers.cend()) {
         return *it;
     }
@@ -88,15 +99,16 @@ shared_ptr<FimdImpl::FimdWatcher> FimdImpl::findFimdWatcherByPid(const int pid) 
 
 char **FimdImpl::getPathArrayFromSubject(const int pid, const FimWatcherSubject subject) {
 	vector<string> pathvec;
-	for (int j = 0; j < subject.paths_size(); ++j) {
+	for_each(subject.path().cbegin(), subject.path().cend(), [&](string path) {
 		stringstream ss;
-		ss << "/proc/" << pid << "/root" << subject.paths(j).c_str();
+		ss << "/proc/" << pid << "/root" << path.c_str();
 		pathvec.push_back(ss.str());
-	}
+	});
+
 	char **patharr = new char *[pathvec.size()];
-	for(size_t x = 0; x < pathvec.size(); x++){
-		patharr[x] = new char[pathvec[x].size() + 1];
-		strcpy(patharr[x], pathvec[x].c_str());
+	for(size_t i = 0; i < pathvec.size(); ++i) {
+		patharr[i] = new char[pathvec[i].size() + 1];
+		strcpy(patharr[i], pathvec[i].c_str());
 	}
 	return patharr;
 }
@@ -105,38 +117,38 @@ uint32_t FimdImpl::getEventMaskFromSubject(const FimWatcherSubject subject) {
 	uint32_t mask = 0;
 	// @TODO: IN_EXCL | IN_DONT_FOLLOW ...
 	// for cases when more than one path is being watched; A,B - where B links to A, etc.
-	for (int j = 0; j < subject.events_size(); ++j) {
-		const char *event = subject.events(j).c_str();
-		if (strcmp(event, "all") == 0)         mask |= IN_ALL_EVENTS;
-		else if (strcmp(event, "access") == 0) mask |= IN_ACCESS;
-		else if (strcmp(event, "modify") == 0) mask |= IN_MODIFY;
-		else if (strcmp(event, "attrib") == 0) mask |= IN_ATTRIB;
-		else if (strcmp(event, "open") == 0)   mask |= IN_OPEN;
-		else if (strcmp(event, "close") == 0)  mask |= IN_CLOSE;
-		else if (strcmp(event, "create") == 0) mask |= IN_CREATE;
-		else if (strcmp(event, "delete") == 0) mask |= IN_DELETE;
-		else if (strcmp(event, "move") == 0)   mask |= IN_MOVE;
-	}
+	for_each(subject.event().cbegin(), subject.event().cend(), [&](string event) {
+		const char *evt = event.c_str();
+		if (strcmp(evt, "all") == 0)         mask |= IN_ALL_EVENTS;
+		else if (strcmp(evt, "access") == 0) mask |= IN_ACCESS;
+		else if (strcmp(evt, "modify") == 0) mask |= IN_MODIFY;
+		else if (strcmp(evt, "attrib") == 0) mask |= IN_ATTRIB;
+		else if (strcmp(evt, "open") == 0)   mask |= IN_OPEN;
+		else if (strcmp(evt, "close") == 0)  mask |= IN_CLOSE;
+		else if (strcmp(evt, "create") == 0) mask |= IN_CREATE;
+		else if (strcmp(evt, "delete") == 0) mask |= IN_DELETE;
+		else if (strcmp(evt, "move") == 0)   mask |= IN_MOVE;
+	});
 	return mask;
 }
 
-void FimdImpl::createInotifyWatcher(const FimWatcherSubject subject, char **patharr, uint32_t event_mask, vector<int> *procFds) {
+void FimdImpl::createInotifyWatcher(const FimWatcherSubject subject, char **patharr, uint32_t event_mask, RepeatedField<int32> *procFds) {
 	// create anonymous pipe to communicate with inotify watcher
 	int procFd = eventfd(0, EFD_CLOEXEC);
 	if (procFd == EOF) {
 		return;
 	}
-	procFds->push_back(procFd);
+	procFds->Add(procFd);
 
 	packaged_task<void(int, char **, uint32_t, int)> task(start_inotify_watcher);
-	thread taskThread(move(task), subject.paths_size(), (char **)patharr, (uint32_t)event_mask, procFd);
+	thread taskThread(move(task), subject.path_size(), (char **)patharr, (uint32_t)event_mask, procFd);
 	taskThread.detach();
 }
 
-void FimdImpl::sendKillSignalToWatcher(shared_ptr<FimdWatcher> watcher) {
+void FimdImpl::sendKillSignalToWatcher(shared_ptr<FimdHandle> watcher) {
 	// kill existing watcher polls
 	uint64_t value = FIMNOTIFY_KILL;
-	for_each(watcher->processEventfds.cbegin(), watcher->processEventfds.cend(), [&](int procFd) {
-		write(procFd, &value, sizeof(value));
+	for_each(watcher->processeventfd().cbegin(), watcher->processeventfd().cend(), [&](int processEventfd) {
+		write(processEventfd, &value, sizeof(value));
 	});
 }
--- fimd_impl.h
+++ fimd_impl.h
@@ -5,6 +5,7 @@
 #include <thread>
 #include <vector>
 
+#include "fimd_util.h"
 #include "fim-proto/c++/fim.grpc.pb.h"
 
 class FimdImpl final : public fim::Fimd::Service {
@@ -12,23 +13,23 @@ public:
     explicit FimdImpl() = default;
     ~FimdImpl() = default;
 
-    struct FimdWatcher {
-        const int pid;
-        std::vector<int> processEventfds;
-    };
-
     grpc::Status CreateWatch(grpc::ServerContext *context, const fim::FimdConfig *request, fim::FimdHandle *response) override;
-    grpc::Status DestroyWatch(grpc::ServerContext *context, const fim::FimdConfig *request, fim::FimdHandle *response) override;
+    grpc::Status DestroyWatch(grpc::ServerContext *context, const fim::FimdConfig *request, fim::Empty *response) override;
 
 private:
-    std::shared_ptr<FimdWatcher> findFimdWatcherByPid(const int pid);
+    std::shared_ptr<fim::FimdHandle> findFimdWatcherByPid(const std::string hostUid, const int pid);
 	char **getPathArrayFromSubject(const int pid, const fim::FimWatcherSubject subject);
 	uint32_t getEventMaskFromSubject(const fim::FimWatcherSubject subject);
-	void createInotifyWatcher(const fim::FimWatcherSubject subject, char **patharr, uint32_t event_mask, std::vector<int> *procFds);
-	void sendKillSignalToWatcher(std::shared_ptr<FimdWatcher> watcher);
+	void createInotifyWatcher(const fim::FimWatcherSubject subject, char **patharr, uint32_t event_mask,
+		::google::protobuf::RepeatedField<::google::protobuf::int32> *procFds);
+	void sendKillSignalToWatcher(std::shared_ptr<fim::FimdHandle> watcher);
+
+	inline const std::string cleanContainerId(const std::string &containerId) const {
+		return FimdUtil::eraseSubstr(containerId, "docker://");
+	}
 
 private:
-    std::vector<std::shared_ptr<FimdWatcher>> m_watchers;
+    std::vector<std::shared_ptr<fim::FimdHandle>> m_watchers;
 };
 
 #endif
